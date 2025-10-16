ee66826c58883aa917add235628665e3fa3f56ea
MdSahill <mpc2742@gmail.com>
2025-10-16 19:51:28 +0530

    hardhat configuration

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,8 +1,18 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.19',
   networks: {
-    hardhat: {}
+    localhost: {
+      url: 'http://127.0.0.1:8545'
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  paths: {
+    artifacts: './backend/build/contracts'
   }
 };
